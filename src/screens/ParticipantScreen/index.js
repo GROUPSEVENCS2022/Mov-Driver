5da739b6c8ed0e5528da3f9aa393bccd5f22c8cb
GROUPSEVENCS2022 <groupsevencs2022@gmail.com>
2023-07-22 23:47:11 -0700

    stager

--- src/screens/ParticipantScreen/index.js
+++ src/screens/ParticipantScreen/index.js
@@ -8,18 +8,7 @@ const ParticipantStatus = ({navigation, route}) => {
 
   // user management
   const { activeUser } = route.params;
-  const userEmail = activeUser[0]?.email;
-
-  // setParticipantsList(participants[0].participants);
-
-  const handlePress = (participant) => {
-    setSelectedParticipant(participant);
-  };
-
-  const handleClose = () => {
-    setSelectedParticipant(null);
-  };
-  
+  const userEmail = activeUser[0]?.email;  
     return (
       <ParticipantList />
     );
